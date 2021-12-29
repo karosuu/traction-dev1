ac14593a8dd7f49060aeeb9728acee9b310e8770
karosuu <54604110+karosuu@users.noreply.github.com>
2021-12-29 15:32:34 -0600

    Add new Variable

--- cypress/page-objects/components/main.js
+++ cypress/page-objects/components/main.js
@@ -5,7 +5,7 @@ passInput() { return cy.get('#Password') }
 loginButton() { return  cy.get('input[type=submit]') }
 
 //Metting objects
-menuMeeting() { return cy.get('#header-tab-l10') }
+navMeeting() { return cy.get('#header-tab-l10') }
 concludeBtn() { return cy.get('#conclude_meeting_button') }
 gotoMeeting() { return cy.get('tr:nth-of-type(1) > .hidden-xs.l10-row-cell.view-meeting-cell > .go-indicator.label.label-default') }
 }
