ab6984a2a35ebc0efde7bd6dcedf2b5a2046fdf4
karosuu <54604110+karosuu@users.noreply.github.com>
2022-01-12 09:02:07 -0600

    Removed demo files and re numbered real tests

--- cypress/page-objects/components/main.js
+++ cypress/page-objects/components/main.js
@@ -7,9 +7,9 @@ export class Main {
 
     //Metting objects
     navMeeting() { return cy.get('#header-tab-l10') }
-    concludeBtn() { return cy.get('#conclude_meeting_button') }
     gotoMeetingBtn() { return cy.get('tr:nth-of-type(1) > .hidden-xs.l10-row-cell.view-meeting-cell > .go-indicator.label.label-default') }
     startMeetingBtn() { return cy.get('#l10-meeting-startbutton') }
+
 
 };
 
