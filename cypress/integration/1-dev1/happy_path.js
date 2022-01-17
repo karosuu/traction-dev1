ffbd7337cd15b2b975c7338d7963d7d235aa51d8
karosuu <54604110+karosuu@users.noreply.github.com>
2022-01-17 16:44:16 -0600

    happy path update

--- cypress/integration/1-dev1/happy_path.js
+++ cypress/integration/1-dev1/happy_path.js
@@ -2,7 +2,7 @@ import { Main } from '../../page-objects/components/main'
 import { emailInputID, testInputID } from '../../page-objects/components/input_utillities'
 
 
-describe('Login and run Meeting', () => {
+describe('Register a new user and Organization', () => {
     const main = new Main()
 
     beforeEach(function () {
@@ -22,17 +22,29 @@ describe('Login and run Meeting', () => {
     });
 
     it('Enter user details', () => {
-        cy.visit('https://dev-1.traction.tools/OnBoarding')
+        cy.visit('https://dev-1.traction.tools/getstarted')
         //cy.url().should('include', '/OnBoarding')
-       // cy.title().should('eq', 'Sign Up - Traction Tools')
+        // cy.title().should('eq', 'Sign Up - Traction Tools')
 
         main.getStartFname().type('Test')
         main.getStartLname().type(testInputID('User '))
-       main.getStartEmail().type(emailInputID('carlos.cuadra+', 'smbssolutions.com'))
+        main.getStartEmail().type(emailInputID('carlos.cuadra+', '@smbssolutions.com'))
+        main.getStartPassword().type('Testuser12345#')
+        main.getStartCompany().type(testInputID('Test Carlos: New Company '))
+        main.getStartCoach().click()
+        main.getStartReferral().select('I Found You Online').should('have.value', 'online')
+        main.getStartSignUpBtn().click()
 
-        main.getStartPassword().type('Testuser12345')
-        main.getStartCoach().click
-       
+
     })
+    it('Approve consent', () => {
+        cy.url().should('include', '/Account/Consent')
+        main.consetOkBtn().click()
+        //main.consentNoBtn().click()
 
+    })
+    it('Close modal', () => {
+        cy.url().should('include', '/Dashboard')
+        cy.get('#pendo-close-guide-4657e67b').click()
+    })
 });
--- cypress/page-objects/components/main.js
+++ cypress/page-objects/components/main.js
@@ -1,18 +1,23 @@
 export class Main {
 
-    //Get started
+    //Getstarted page objects
     getStartFname() { return cy.get('#firstName') }
     getStartLname() { return cy.get('#lastName') }
     getStartEmail() { return cy.get('#email') }
     getStartPassword() { return cy.get('#password') }
+    getStartCompany () { return cy.get('#companyName') }
     getStartCoach() { return cy.get('#implementerNo') }
-
-    //Login objects
+    getStartReferral() { return cy.get('#referralSource') }
+    getStartSignUpBtn() { return cy.get('.Login-btn') }
+    consetOkBtn() {  return cy.get('form button.btn-primary') }
+    consentNoBtn() { return cy.get('form button.btn-link') }
+    
+    //Login  page objects
     loginInput() { return cy.get('#email') }
     passInput() { return cy.get('#password') }
     loginButton() { return cy.get('.Login-btn') }
 
-    //Metting objects
+    //Metting page objects
     navMeeting() { return cy.get('#header-tab-l10') }
     gotoMeetingBtn() { return cy.get('tr:nth-of-type(1) > .hidden-xs.l10-row-cell.view-meeting-cell > .go-indicator.label.label-default') }
     startMeetingBtn() { return cy.get('#l10-meeting-startbutton') }
